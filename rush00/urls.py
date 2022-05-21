b64901eac9496a4282f7d5a7aa20ee180bc208fe
Michael <msnazarow@gmail.com>
2022-05-21 22:00:21 +0300

    feat: apps

--- rush00/urls.py
+++ rush00/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('worldmap/', include('worldmap.urls')),
+    path('moviedex/', include('movidex.urls')),
+    path('options/', include('options.urls')),
+    path('battle/', include('battle.urls'))
 ]
