afe07e4c01ca226979a0829d761b443a9135b741
Michael <msnazarow@gmail.com>
2022-05-22 11:15:38 +0300

    fix: tab to spaces

--- Moviemon/GameManager.py
+++ Moviemon/GameManager.py
@@ -6,58 +6,58 @@ from settings import DEFAULT_PLAYER_POSITION, MAP_SIZE, MOVIE_IDS, DEFAULT_PLAYE
 
 @dataclass
 class Moviemon:
-	id: int
-	name: str
+    id: int
+    name: str
 
 
 @dataclass
 class GameData:
-	player_strength: int
-	player_position: (int, int)
-	player_movieballs: int
-	captured_moviemon_ids: {int}
-	movie_info: {int: Moviemon}
+    player_strength: int
+    player_position: (int, int)
+    player_movieballs: int
+    captured_moviemon_ids: {int}
+    movie_info: {int: Moviemon}
 
 
 class GameManager:
 
-	def __init__(self):
-		self._game_data = GameData(DEFAULT_PLAYER_STRENGTH, DEFAULT_PLAYER_POSITION, 0, {}, {})
+    def __init__(self):
+        self._game_data = GameData(DEFAULT_PLAYER_STRENGTH, DEFAULT_PLAYER_POSITION, 0, {}, {})
 
-	def load(self, game_data: GameData):
-		self._game_data = game_data
-		return self
+    def load(self, game_data: GameData):
+        self._game_data = game_data
+        return self
 
-	def dump(self) -> GameData:
-		return self._game_data
+    def dump(self) -> GameData:
+        return self._game_data
 
-	def get_random_movie(self) -> Moviemon:
-		return random.choice(MOVIE_IDS - self._game_data.captured_moviemon_ids)
+    def get_random_movie(self) -> Moviemon:
+        return random.choice(MOVIE_IDS - self._game_data.captured_moviemon_ids)
 
-	def get_strength(self) -> int:
-		return self._game_data.player_strength
+    def get_strength(self) -> int:
+        return self._game_data.player_strength
 
-	def load_default_settings(self) -> 'GameManager':
-		self._game_data = GameData(DEFAULT_PLAYER_STRENGTH, DEFAULT_PLAYER_POSITION, 0, {}, {})
-		return self
+    def load_default_settings(self) -> 'GameManager':
+        self._game_data = GameData(DEFAULT_PLAYER_STRENGTH, DEFAULT_PLAYER_POSITION, 0, {}, {})
+        return self
 
-	def get_movie(self, id: int):
-		return self._game_data.movie_info[id]
+    def get_movie(self, id: int):
+        return self._game_data.movie_info[id]
 
-	def save(self, slot_name):
-		with open(f"slot{slot_name}_{len(self._game_data.captured_moviemon_ids)}_{len(MOVIE_IDS)}.mmg",
-		          "wb") as save_file:
-			pickle.dump(self.dump(), save_file)
+    def save(self, slot_name):
+        with open(f"slot{slot_name}_{len(self._game_data.captured_moviemon_ids)}_{len(MOVIE_IDS)}.mmg",
+                  "wb") as save_file:
+            pickle.dump(self.dump(), save_file)
 
-	def quick_save(self):
-		with open("current_game.mmg", "wb") as current_game:
-			pickle.dump(self.dump(), current_game)
+    def quick_save(self):
+        with open("current_game.mmg", "wb") as current_game:
+            pickle.dump(self.dump(), current_game)
 
-	def load_file(self, save_file):
-		with open(save_file, "rb") as save_file:
-			self.load(pickle.load(save_file))
+    def load_file(self, save_file):
+        with open(save_file, "rb") as save_file:
+            self.load(pickle.load(save_file))
 
 
 
 if __name__ == '__main__':
-	game_manager = GameManager()
+    game_manager = GameManager()
--- mainpage/views.py
+++ mainpage/views.py
@@ -3,4 +3,4 @@ from django.views import generic
 
 
 class IndexView(generic.TemplateView):
-	template_name = 'mainpage/index.html'
+    template_name = 'mainpage/index.html'
