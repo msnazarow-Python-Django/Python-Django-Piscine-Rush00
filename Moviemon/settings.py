9234684903785c7bbacb7f34fd1d20814c0828a4
mazor <mazor@student.21-school.ru>
2022-05-24 04:30:57 +0400

    Added middleware for current game saving after every button pressing

--- Moviemon/settings.py
+++ Moviemon/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middleware.SaveStateMiddleware.SaveStateMiddleware'
 ]
 
 ROOT_URLCONF = 'Moviemon.urls'
